b61521e774b75c29a77a36f9f67922e0a944d1fb
Charly <charly91.3ds@gmail.com>
2022-12-25 20:58:34 -0300

    iniciando html login

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', (req, res) => {
+    res.send('Holis Veronikos');
+    }
+);
+
+//app get a la ruta login.html
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname , '/views/login.html'));
+    }
+);
+
+
+app.listen(port, () => {
+    console.log(`servidor levantado en http://localhost:${port}`);
+    }
+);
+
+
+
