57bec74c9ee0e30a9f6e91e342cf04fb65beee93
Jorge Nicolas Sosa <nicoomg96@gmail.com>
2023-01-10 23:56:26 -0300

    vista register

--- app.js
+++ app.js
@@ -17,6 +17,11 @@ app.get('/login', (req, res) => {
     }
 );
 
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname , '/views/register.html'));
+    }
+);
+
 
 app.listen(port, () => {
     console.log(`servidor levantado en http://localhost:${port}`);
