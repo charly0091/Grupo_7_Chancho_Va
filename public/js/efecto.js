1829e53434e9a8634932533b04f9ca2c263775fd
Jorge Nicolas Sosa <nicoomg96@gmail.com>
2023-02-01 22:48:33 -0300

    Register corregido + nuevo header

--- public/js/efecto.js
+++ public/js/efecto.js
@@ -2,8 +2,11 @@ let barra = document.querySelector(".barra")
 let linea1 = document.querySelector(".linea1");
 let linea2 = document.querySelector(".linea2");
 let linea3 = document.querySelector(".linea3");
-let link = document.querySelector(".prueba");
-let menu = document.querySelector(".menu")
+let link = document.querySelector(".contenedorOculto");
+let menu = document.querySelector(".palabra")
+let producto = document.querySelector(".categoriaProductos")
+let iconoBuscardor = document.querySelector(".iconoBuscador")
+let barraBuscadora = document.querySelector(".barraBuscadora")
 
 
 barra.addEventListener("click", () => {
@@ -11,7 +14,12 @@ barra.addEventListener("click", () => {
     linea2.classList.toggle("activelinea2");
     linea3.classList.toggle("activelinea3");
     link.classList.toggle("active")
-    menu.classList.toggle("active")
 });
 
+menu.addEventListener("click",() => {
+    producto.classList.toggle("active")
+})
 
+iconoBuscardor.addEventListener("click",() => {
+    barraBuscadora.classList.toggle("active")
+})
