3b43306c38338d97a8a9d619451a0443b4d52368
Jorge Nicolas Sosa <nicoomg96@gmail.com>
2023-02-02 22:47:20 -0300

    Base de datos e imagenes

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ app.use(express.static("public"));
 /* Template engine config */
 app.set('view engine', 'ejs');
 app.set('views', "./src/views");
+app.set('views', "./src/views/users")
 
 /* Routers */
 const mainRouter = require('./routes/main');
