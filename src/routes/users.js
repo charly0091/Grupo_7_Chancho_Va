0b048183019080eafcedefdfcd8952230d7263a3
Jorge Nicolas Sosa <nicoomg96@gmail.com>
2023-02-14 15:22:27 -0300

    creacion de metodos de pago y pago con targeta

--- src/routes/users.js
+++ src/routes/users.js
@@ -7,5 +7,7 @@ router.get("/register", controller.register);
 router.post("/", controller.crear);
 router.get("/login", controller.login);
 router.get("/reset-password", controller.resetPassword);
+router.get("/metodosDePago", controller.metodosDePago);
+router.get("/pagoTargeta", controller.pago)
 
 module.exports = router;
