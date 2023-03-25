5bc431f5c52f11ed19d05a105035fd003b9befbb
Charly <charly91.3ds@gmail.com>
2023-03-25 18:38:03 -0300

    arreglado error que faltaba funcion writeJSON

--- src/data/index.js
+++ src/data/index.js
@@ -7,6 +7,9 @@ module.exports = {
     readJSON : (json) => {
         return JSON.parse(fs.readFileSync(path.resolve(__dirname, json)))
     },
+    writeJSON : (json, array) => {
+        fs.writeFileSync(path.resolve(__dirname, json), JSON.stringify(array,null,3), 'utf-8')
+    },
     writeUsersJson: (data) => {
         fs.writeFileSync(path.join(__dirname, "../data/usersDataBase.json"), JSON.stringify(data), "utf-8")
     },
